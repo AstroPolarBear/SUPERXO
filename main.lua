b90eb45006c5ca6fbd2cc0ce961a81fdee14bfb6
AstroPolarBear <abdullah230102@gmail.com>
2024-06-27 16:31:55 +0400

    Created a grid to be played on.

--- main.lua
+++ main.lua
@@ -1,54 +1,42 @@
+if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
+    require("lldebugger").start()
+  end
+
 function love.load()
-    anim8 = require 'libraries/anim8'
-    love.graphics.setDefaultFilter("nearest", "nearest")
-
-    player = {}
-    player.x = 400
-    player.y = 200
-    player.speed = 1
-    player.spriteSheet = love.graphics.newImage('sprites/bat.png')
-    player.grid = anim8.newGrid( 32, 32, player.spriteSheet:getWidth(), player.spriteSheet:getHeight())
-
-    player.animations = {}
-    player.animations.down = anim8.newAnimation( player.grid('2-4',1), 0.2)
-    player.animations.right = anim8.newAnimation( player.grid('2-4',2), 0.2)
-    player.animations.up = anim8.newAnimation( player.grid('2-4',3), 0.2)
-    player.animations.left = anim8.newAnimation( player.grid('2-4',4), 0.2)
-
-    player.anim = player.animations.left
+    
 
 end
 
-
 function love.update(dt)
-    local isMoving = false
 
-    if love.keyboard.isDown("right") then
-        player.x = player.x + player.speed
-        player.anim = player.animations.right
-        isMoving = true
-    end
-    if love.keyboard.isDown("up") then
-        player.y = player.y - player.speed
-        player.anim = player.animations.up
-        isMoving = true
-    end
-    if love.keyboard.isDown("left") then
-        player.x = player.x - player.speed
-        player.anim = player.animations.left
-        isMoving = true
-    end
-    if love.keyboard.isDown("down") then
-        player.y = player.y + player.speed
-        player.anim = player.animations.down
-        isMoving = true
-    end
-
-    player.anim:update(dt)
 end
 
 
 function love.draw()
-    love.graphics.setBackgroundColor(0.5,0,1)
-    player.anim:draw(player.spriteSheet, player.x, player.y, nil, 3, 3)
+    local window_w , window_h = love.graphics.getDimensions()
+    local box_l = 0.6 * window_w
+    local box_x, box_y = (window_w * 0.2), (window_h * 0.1)
+
+    love.graphics.setColor(0,0,1)
+    love.graphics.rectangle("fill", box_x, box_y, box_l, box_l)
+
+    local slot_l = 0.3 * box_l
+    local slot_x = box_x
+    local slot_y = box_y
+
+    love.graphics.setColor(1,1,1)
+    for i = 1, 3, 1 do
+        for j = 1, 3, 1 do
+            love.graphics.rectangle("fill", slot_x, slot_y, slot_l, slot_l)
+            slot_x = slot_x + slot_l + (0.05 * box_l)            
+        end
+        slot_x = box_x
+        slot_y = slot_y + slot_l + (0.05 * box_l)
+    end
+end
+
+function love.mousepressed(mx, my, button)
+    if button == 1 and then
+        
+    end
 end
